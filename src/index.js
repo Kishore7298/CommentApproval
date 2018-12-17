72be2bb6ed18a8f6234f3875c600685c4f792011
kishore <4nm15is025@nmamit.in>
2018-12-17 17:54:26 +0530

    doing it in the rigth way!

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
+import ComponentDetail from './ComponentDetail'
 
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a className="avatar">
-                    <img alt="avatar" src={Faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00 pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+           <ComponentDetail />
+           <ComponentDetail />
+           <ComponentDetail />
+           <ComponentDetail />
         </div>
     )
 };
