5778de036bf54ae1e93775d645f552b5c99cd747
kishore <4nm15is025@nmamit.in>
2018-12-20 19:02:44 +0530

    passing avatar as props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import ComponentDetail from './ComponentDetail'
 const App = ()=>{
     return (
         <div className="ui container comments">
-           <ComponentDetail  author="sam" timeAgo="Today at 4:45pm" content="It's very nice!"/>
-           <ComponentDetail author="will" timeAgo="Today at 5:00pm" content="I like that novel"/>
-           <ComponentDetail author="bran" timeAgo="Yesterday at 6:00pm" content="I liked the writing!"/>
+           <ComponentDetail  author="sam" timeAgo="Today at 4:45pm" content="It's very nice!" avatar={Faker.image.avatar()}/>
+           <ComponentDetail author="will" timeAgo="Today at 5:00pm" content="I like that novel" avatar={Faker.image.avatar()}/>
+           <ComponentDetail author="bran" timeAgo="Yesterday at 6:00pm" content="I liked the writing!" avatar={Faker.image.avatar()}/>
         </div>
     )
 };
