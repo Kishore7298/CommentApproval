d31d10f8109fe790cbd6cf60ceed61da9919d93d
kishore <4nm15is025@nmamit.in>
2018-12-20 19:47:22 +0530

    Passing the child as props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
-import ComponentDetail from './ComponentDetail'
+import ComponentDetail from './ComponentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = ()=>{
     return (
         <div className="ui container comments">
-           <ComponentDetail  author="sam" timeAgo="Today at 4:45pm" content="It's very nice!" avatar={Faker.image.avatar()}/>
-           <ComponentDetail author="will" timeAgo="Today at 5:00pm" content="I like that novel" avatar={Faker.image.avatar()}/>
-           <ComponentDetail author="bran" timeAgo="Yesterday at 6:00pm" content="I liked the writing!" avatar={Faker.image.avatar()}/>
+        <ApprovalCard>
+            <ComponentDetail  author="sam" timeAgo="Today at 4:45pm" content="It's very nice!" avatar={Faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+            <ComponentDetail author="will" timeAgo="Today at 5:00pm" content="I like that novel" avatar={Faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+            <ComponentDetail author="bran" timeAgo="Yesterday at 6:00pm" content="I liked the writing!" avatar={Faker.image.avatar()}/>
+        </ApprovalCard>  
+        <ApprovalCard>
+            <ComponentDetail author="bran" timeAgo="Yesterday at 6:00pm" content="I liked the writing!" avatar={Faker.image.avatar()}/>
+        </ApprovalCard>
         </div>
     )
 };
